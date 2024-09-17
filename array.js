8c78aa71998c8e5d843c95bab6a9376cdeeb5fd9
Aarti Chauhan <aarticahauhan20@gmail.com>
2024-09-17 17:32:18 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,9 @@
+// javaScript method toString() converts an array to a string of array values.
+const fruits = ["Apple", "Banana", "Grapes", "Mango", "Cucumber", "Orange", "Kivi"];
+document.getElementById("demo").innerHTML = fruits.toString();
+
+// WE can also creates arrays using new keyword.
+const cars = new  Array("Saas", "Volvo", "BMW");
+
+// access the full array
+console.log(cars);
